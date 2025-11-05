86482d0c041d102e1a741ef875f1a029de87ba37
GarrettThing <garrett.clark0103@gmail.com>
2025-11-05 08:28:04 -0600

    Created a list of lists of the data :)

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
+import csv
 
 def main():
-    pass
+    ## where the data is held
+    data_set = []
+
+    with open('ball_positions.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in csvreader:
+            # creates the data and puts it in the data_set
+            data_set.append(row)
+
 
 if __name__ == "__main__":
     main()
