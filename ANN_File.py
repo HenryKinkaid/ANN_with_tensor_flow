d808aeaf531783fb18ec910044dca9b668402102
henry <henry.silva@kinkaid.org>
2025-11-07 09:15:15 -0600

    added a class to handle the ANN

---
+++ ANN_File.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import keras
+from keras import layers
+
+class ANN():
+    def __init__(self):
+        # define the model with keras
+
+        self.model = keras.Sequential(
+            [
+                layers.Dense(6, activation="relu", name="layer1"),
+                layers.Dense(6, activation="relu", name="layer2"),
+                layers.Dense(6, activation="relu", name="layer3"),
+                layers.Dense(2, name="layer4"),
+            ]
+        )
+
+    def test_model(self, inputs: list):
+        return self.model(inputs)
+
+    def train_model_n_epochs(self, n):
+        pass
